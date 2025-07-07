aa94edaa0fe0ac3b3f043bf3cff8fde329998bc4
Denis Carriere <carriere.denis@gmail.com>
2025-07-06 22:01:27 -0400

    Update pumpfun

--- clickhouse-solana-dex/src/pumpfun.rs
+++ clickhouse-solana-dex/src/pumpfun.rs
@@ -1,25 +1,42 @@
 use common::clickhouse::{common_key_v2, set_clock, set_pumpfun_instruction_v2, set_pumpfun_transaction_v2};
 use proto::pb::pumpfun::v1 as pb;
 use substreams::pb::substreams::Clock;
+use substreams_database_change::tables::Row;
 use substreams_solana::base58;
 
 pub fn process_events(tables: &mut substreams_database_change::tables::Tables, clock: &Clock, events: &pb::Events) {
     for (transaction_index, transaction) in events.transactions.iter().enumerate() {
         for (instruction_index, instruction) in transaction.instructions.iter().enumerate() {
-            // Get TradeEvent from the next instruction
-            let event = match transaction.instructions[instruction_index + 1].instruction {
-                Some(pb::instruction::Instruction::Trade(ref event)) => event,
-                _ => panic!("{} signature: {}", instruction_index, base58::encode(&transaction.signature)), // Skip if not a TradeEvent
-            };
-
             match &instruction.instruction {
                 Some(pb::instruction::Instruction::Buy(data)) => {
+                    let Some(event) = get_trade_event(transaction, instruction_index) else {
+                        continue;
+                    };
+                    substreams::log::info!("buy [{}]: {:?}", instruction_index, base58::encode(&transaction.signature));
                     handle_buy(tables, clock, transaction, instruction, data, event, transaction_index, instruction_index);
+                }
+                Some(pb::instruction::Instruction::Sell(data)) => {
+                    let Some(event) = get_trade_event(transaction, instruction_index) else {
+                        continue;
+                    };
+                    substreams::log::info!("sell [{}]: {:?}", instruction_index, base58::encode(&transaction.signature));
+                    handle_sell(tables, clock, transaction, instruction, data, event, transaction_index, instruction_index);
                 }
 
                 _ => {}
             }
         }
+    }
+}
+
+fn get_trade_event(transaction: &pb::Transaction, instruction_index: usize) -> Option<&pb::TradeEvent> {
+    if instruction_index + 1 < transaction.instructions.len() {
+        match &transaction.instructions[instruction_index + 1].instruction {
+            Some(pb::instruction::Instruction::Trade(event)) => Some(event),
+            _ => None,
+        }
+    } else {
+        None
     }
 }
 
@@ -40,8 +57,43 @@ fn handle_buy(
     let key = common_key_v2(&clock, transaction_index, instruction_index);
     let row = tables
         .create_row("pumpfun_buy", key)
-        // accounts
-        .set("global", base58::encode(&accounts.mint))
+        .set("amount", data.amount)
+        .set("max_sol_cost", data.max_sol_cost);
+
+    set_trade_event(event, accounts, row);
+    set_pumpfun_instruction_v2(instruction, row);
+    set_pumpfun_transaction_v2(transaction, row);
+    set_clock(clock, row);
+}
+
+fn handle_sell(
+    tables: &mut substreams_database_change::tables::Tables,
+    clock: &Clock,
+    transaction: &pb::Transaction,
+    instruction: &pb::Instruction,
+    data: &pb::SellInstruction,
+    event: &pb::TradeEvent,
+    transaction_index: usize,
+    instruction_index: usize,
+) {
+    let accounts = match &data.accounts {
+        Some(accounts) => accounts,
+        None => return,
+    };
+    let key = common_key_v2(&clock, transaction_index, instruction_index);
+    let row = tables
+        .create_row("pumpfun_sell", key)
+        .set("amount", data.amount)
+        .set("min_sol_output", data.min_sol_output);
+
+    set_trade_event(event, accounts, row);
+    set_pumpfun_instruction_v2(instruction, row);
+    set_pumpfun_transaction_v2(transaction, row);
+    set_clock(clock, row);
+}
+
+fn set_trade_event(event: &pb::TradeEvent, accounts: &pb::TradeAccounts, row: &mut Row) {
+    row.set("global", base58::encode(&accounts.mint))
         .set("fee_recipient", base58::encode(&accounts.fee_recipient))
         .set("mint", base58::encode(&accounts.mint))
         .set("bonding_curve", base58::encode(&accounts.bonding_curve))
@@ -49,9 +101,6 @@ fn handle_buy(
         .set("associated_user", base58::encode(&accounts.associated_user))
         .set("user", base58::encode(&accounts.user))
         .set("creator_vault", base58::encode(&accounts.creator_vault))
-        // data
-        .set("amount", data.amount)
-        .set("max_sol_cost", data.max_sol_cost)
         // event
         .set("mint", base58::encode(&event.mint))
         .set("sol_amount", event.sol_amount)
@@ -70,54 +119,7 @@ fn handle_buy(
         )
         .set("fee_basis_points", event.fee_basis_points.unwrap_or(0))
         .set("fee", event.fee.unwrap_or(0))
-        // .set("creator", base58::encode(&event.creator))
         .set("creator", event.creator.as_ref().map_or_else(|| "".to_string(), |c| base58::encode(c)))
         .set("creator_fee_basis_points", event.creator_fee_basis_points.unwrap_or(0))
         .set("creator_fee", event.creator_fee.unwrap_or(0));
-
-    set_pumpfun_instruction_v2(instruction, row);
-    set_pumpfun_transaction_v2(transaction, row);
-    set_clock(clock, row);
 }
-
-// // One emitted trade (buy or sell) on a Pump.fun bonding curve.
-// message TradeEvent {
-//   // 32-byte SPL-Token mint address.
-//   bytes  mint                       = 1;
-
-//   // Lamports moved (positive on buys, negative on sells).
-//   uint64 sol_amount                 = 2;
-
-//   // Token amount moved (positive on buys, negative on sells).
-//   uint64 token_amount               = 3;
-
-//   // true = buy (SOL→SPL), false = sell.
-//   bool   is_buy                     = 4;
-
-//   // Trader’s wallet (32 bytes).
-//   bytes  user                       = 5;
-
-//   // Unix-epoch seconds.
-//   int64  timestamp                  = 6;
-
-//   uint64 virtual_sol_reserves       = 7;
-//   uint64 virtual_token_reserves     = 8;
-//   uint64 real_sol_reserves          = 9;
-//   uint64 real_token_reserves        = 10;
-
-//   // Protocol-fee recipient (32 bytes).
-//   optional bytes  fee_recipient              = 11;
-//   optional uint64 fee_basis_points           = 12; // basis-points, 1 bp = 0.01 %
-//   optional uint64 fee                        = 13; // lamports
-
-//   // Pool creator wallet (32 bytes).
-//   optional bytes  creator                    = 14;
-//   optional uint64 creator_fee_basis_points   = 15;
-//   optional uint64 creator_fee                = 16; // lamports
-// }
-
-// message BuyInstruction {
-//     TradeAccounts accounts = 1;
-//     uint64 amount = 2;
-//     uint64 max_sol_cost = 3;
-// }
