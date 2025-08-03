3c4ddfc2980c175ddf13bea092f5847f23f62c96
Denis Carriere <carriere.denis@gmail.com>
2025-08-03 22:05:47 +0200

    update examples

--- clickhouse-solana-tokens/examples/PROJECTION.sql
+++ clickhouse-solana-tokens/examples/PROJECTION.sql
@@ -5,9 +5,18 @@ SELECT
     sumIf(rows, active) AS rows,
     round(sumIf(bytes_on_disk, active) / 1048576) AS mb
 FROM system.parts
-WHERE (database = currentDatabase()) AND (partition = '202204')
+WHERE (database = currentDatabase())
 GROUP BY
     `table`,
     partition
 ORDER BY partition DESC
 LIMIT 30
+
+EXPLAIN indexes =1, projections = 1
+SELECT *
+FROM system_post_balances
+WHERE _part_starting_offset + _part_offset IN (
+    SELECT _part_starting_offset + _part_offset
+    FROM system_post_balances
+    WHERE signature = (SELECT signature FROM system_post_balances ORDER BY rand() LIMIT 1)
+);
