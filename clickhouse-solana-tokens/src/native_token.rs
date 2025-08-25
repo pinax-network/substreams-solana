1da7d60ece86b129f585666dcbc293310e08aa4a
Yaro Shkvorets <shkvorets@gmail.com>
2025-08-25 11:12:17 -0400

    Skip unchanged balances (#23)
    * skip unchanged balances
    
    * log message

--- clickhouse-solana-tokens/src/native_token.rs
+++ clickhouse-solana-tokens/src/native_token.rs
@@ -7,12 +7,19 @@ use substreams_solana::base58;
 
 pub fn process_events(tables: &mut substreams_database_change::tables::Tables, clock: &Clock, events: &pb::Events) {
     let mut system_post_balances_per_block = HashMap::new();
+    let mut system_pre_balances_per_block = HashMap::new();
     for (transaction_index, transaction) in events.transactions.iter().enumerate() {
         // Native Token Balances
-        // Only keep last post balance change per block
+        // Keep first pre balance and last post balance per account
         for (i, post_balance) in transaction.post_balances.iter().enumerate() {
             let key = post_balance.account.as_slice();
             system_post_balances_per_block.insert(key, (post_balance, transaction, transaction_index, i));
+        }
+        for (i, pre_balance) in transaction.pre_balances.iter().enumerate() {
+            let key = pre_balance.account.as_slice();
+            if !system_pre_balances_per_block.contains_key(&key) {
+                system_pre_balances_per_block.insert(key, (pre_balance, transaction, transaction_index, i));
+            }
         }
         // Native Token Instructions
         for (i, instruction) in transaction.instructions.iter().enumerate() {
@@ -36,9 +43,18 @@ pub fn process_events(tables: &mut substreams_database_change::tables::Tables, c
             }
         }
     }
+    let mut skipped = 0;
     for (post_balance, transaction, transaction_index, i) in system_post_balances_per_block.values() {
+        if let Some((pre_balance, _, _, _)) = system_pre_balances_per_block.get(&(post_balance.account.as_slice())) {
+            // Roughly 80% of balances don't change - no need to include these - just skip them
+            if pre_balance.amount == post_balance.amount {
+                skipped += 1;
+                continue;
+            }
+        }
         handle_balances("system_post_balances", tables, clock, transaction, post_balance, *transaction_index, *i);
     }
+    substreams::log::info!("Skipped {} out of {} native token balances", skipped, system_post_balances_per_block.len());
 }
 
 fn handle_transfer(
