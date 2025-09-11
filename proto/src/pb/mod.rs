20011f55d4283c4cca681d33ae408556fbf9614e
Denis Carriere <carriere.denis@gmail.com>
2025-09-11 11:58:59 -0400

    fix meteora

--- proto/src/pb/mod.rs
+++ proto/src/pb/mod.rs
@@ -23,6 +23,20 @@ pub mod meteora {
             // @@protoc_insertion_point(meteora.amm.v1)
         }
     }
+    pub mod daam {
+        // @@protoc_insertion_point(attribute:meteora.daam.v1)
+        pub mod v1 {
+            include!("meteora.daam.v1.rs");
+            // @@protoc_insertion_point(meteora.daam.v1)
+        }
+    }
+    pub mod dllm {
+        // @@protoc_insertion_point(attribute:meteora.dllm.v1)
+        pub mod v1 {
+            include!("meteora.dllm.v1.rs");
+            // @@protoc_insertion_point(meteora.dllm.v1)
+        }
+    }
 }
 pub mod pumpfun {
     pub mod amm {
@@ -68,15 +82,6 @@ pub mod raydium {
         }
     }
 }
-pub mod meteora {
-    pub mod dllm {
-        // @@protoc_insertion_point(attribute:meteora.dllm.v1)
-        pub mod v1 {
-            include!("meteora.dllm.v1.rs");
-            // @@protoc_insertion_point(meteora.dllm.v1)
-        }
-    }
-}
 pub mod solana {
     pub mod metaplex {
         // @@protoc_insertion_point(attribute:solana.metaplex.v1)
@@ -101,16 +106,6 @@ pub mod solana {
                 include!("solana.spl.token.v1.rs");
                 // @@protoc_insertion_point(solana.spl.token.v1)
             }
-        }
-    }
-}
-
-pub mod meteora {
-    pub mod daam {
-        // @@protoc_insertion_point(attribute:meteora.daam.v1)
-        pub mod v1 {
-            include!("meteora.daam.v1.rs");
-            // @@protoc_insertion_point(meteora.daam.v1)
         }
     }
 }
