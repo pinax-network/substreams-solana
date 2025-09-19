d5eb6de284974d0b7d7e4a1b9908ce33a2e3346f
Denis <carriere.denis@gmail.com>
2025-09-19 15:20:07 -0400

    Update metadata views (#58)
    * Update metadata views
    
    * Update combined view
    
    * bump version v0.2.6

--- clickhouse-solana-tokens/schema.3.view.metadata.sql
+++ clickhouse-solana-tokens/schema.3.view.metadata.sql
@@ -1,31 +1,71 @@
-CREATE OR REPLACE VIEW metadata_view AS
-WITH
-  ua AS (SELECT metadata, argMax(update_authority, version) AS update_authority FROM metadata_update_authority_state_latest GROUP BY metadata),
-  mt AS (SELECT metadata, argMax(mint, version) AS mint FROM metadata_mint_state_latest GROUP BY metadata),
-  ma AS (SELECT metadata, argMax(mint_authority, version) AS mint_authority FROM metadata_mint_authority_state_latest GROUP BY metadata),
-  nm AS (SELECT metadata, argMax(name, version) AS name FROM metadata_name_state_latest GROUP BY metadata),
-  sb AS (SELECT metadata, argMax(symbol, version) AS symbol FROM metadata_symbol_state_latest GROUP BY metadata),
-  ur AS (SELECT metadata, argMax(uri, version) AS uri FROM metadata_uri_state_latest GROUP BY metadata)
+/* NAME */
+CREATE OR REPLACE VIEW metadata_name_view AS
 SELECT
-  acc.metadata AS metadata,
-  ua.update_authority,
-  mt.mint,
-  ma.mint_authority,
-  nm.name,
-  sb.symbol,
-  ur.uri
-FROM
-  (
-    SELECT metadata FROM metadata_update_authority_state_latest
-    UNION DISTINCT SELECT metadata FROM metadata_mint_state_latest
-    UNION DISTINCT SELECT metadata FROM metadata_mint_authority_state_latest
-    UNION DISTINCT SELECT metadata FROM metadata_name_state_latest
-    UNION DISTINCT SELECT metadata FROM metadata_symbol_state_latest
-    UNION DISTINCT SELECT metadata FROM metadata_uri_state_latest
-  ) AS acc
-LEFT JOIN ua ON ua.metadata = acc.metadata
-LEFT JOIN mt ON mt.metadata = acc.metadata
-LEFT JOIN ma ON ma.metadata = acc.metadata
-LEFT JOIN nm ON nm.metadata = acc.metadata
-LEFT JOIN sb ON sb.metadata = acc.metadata
-LEFT JOIN ur ON ur.metadata = acc.metadata;
+  metadata,
+  max(version) as version,
+  max(block_num) as block_num,
+  max(timestamp) as timestamp,
+  argMax(name, m.version) AS name
+FROM metadata_name_state_latest as m
+GROUP BY metadata;
+
+/* SYMBOL */
+CREATE OR REPLACE VIEW metadata_symbol_view AS
+SELECT
+  metadata,
+  max(version) as version,
+  max(block_num) as block_num,
+  max(timestamp) as timestamp,
+  argMax(symbol, m.version) AS symbol
+FROM metadata_symbol_state_latest as m
+GROUP BY metadata;
+
+/* URI */
+CREATE OR REPLACE VIEW metadata_uri_view AS
+SELECT
+  metadata,
+  max(version) as version,
+  max(block_num) as block_num,
+  max(timestamp) as timestamp,
+  argMax(uri, m.version) AS uri
+FROM metadata_uri_state_latest as m
+GROUP BY metadata;
+
+/* MINT AUTHORITY */
+CREATE OR REPLACE VIEW metadata_mint_authority_view AS
+SELECT
+  metadata,
+  max(version) as version,
+  max(block_num) as block_num,
+  max(timestamp) as timestamp,
+  argMax(mint_authority, m.version) AS mint_authority
+FROM metadata_mint_authority_state_latest as m
+GROUP BY metadata;
+
+/* UPDATE AUTHORITY */
+CREATE OR REPLACE VIEW metadata_update_authority_view AS
+SELECT
+  metadata,
+  max(version) as version,
+  max(block_num) as block_num,
+  max(timestamp) as timestamp,
+  argMax(update_authority, m.version) AS update_authority
+FROM metadata_update_authority_state_latest as m
+GROUP BY metadata;
+
+/* COMBINED VIEW */
+CREATE OR REPLACE VIEW metadata_view AS
+SELECT
+    k.mint as mint,
+    k.metadata as metadata,
+    k.block_num as block_num,
+    k.timestamp as timestamp,
+    if(empty(n.name), NULL, n.name) AS name,
+    if(empty(s.symbol), NULL, s.symbol) AS symbol,
+    if(empty(u.uri), NULL, u.uri) AS uri
+FROM metadata_mint_state_latest AS k
+    LEFT JOIN metadata_name_view   AS n USING (metadata)
+    LEFT JOIN metadata_symbol_view AS s USING (metadata)
+    LEFT JOIN metadata_uri_view    AS u USING (metadata)
+    LEFT JOIN metadata_mint_authority_view AS ma USING (metadata)
+    LEFT JOIN metadata_update_authority_view AS ua USING (metadata);
