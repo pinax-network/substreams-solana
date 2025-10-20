98b18289b2806d28d88647ceb211357019d9d8d6
Denis Carriere <carriere.denis@gmail.com>
2025-10-19 21:33:20 -0400

    add hour

--- clickhouse-solana-tokens/schema.2.mv.accounts_by_date.sql
+++ clickhouse-solana-tokens/schema.2.mv.accounts_by_date.sql
@@ -1,19 +1,22 @@
--- Transfers activity by Account & Date --
-CREATE TABLE IF NOT EXISTS transfers_activity (
+-- Accounts interactions by Date --
+CREATE TABLE IF NOT EXISTS accounts_by_date (
     account                 String,
     date                    Date,
+    hour                    UInt32,
 
-    INDEX idx_date          (date) TYPE minmax GRANULARITY 1
+    INDEX idx_date          (date) TYPE minmax GRANULARITY 1,
+    INDEX idx_hour          (hour) TYPE minmax GRANULARITY 1
 )
 ENGINE = ReplacingMergeTree
-ORDER BY (account, date)
-COMMENT 'Transfers activity by account & date';
+ORDER BY (account, date, hour)
+COMMENT 'Accounts interactions by date';
 
-CREATE MATERIALIZED VIEW IF NOT EXISTS mv_transfers_activity
-TO transfers_activity
+CREATE MATERIALIZED VIEW IF NOT EXISTS mv_accounts_by_date
+TO accounts_by_date
 AS
 SELECT
     toDate(timestamp) AS date,
+    toRelativeHourNum(timestamp) AS hour,
     arrayJoin(
         arrayDistinct(
             arrayFilter(s -> isNotNull(s) AND s != '',
@@ -29,4 +32,4 @@ SELECT
         )
     ) AS account
 FROM transfers
-GROUP BY date, account;
+GROUP BY account, date, hour;
